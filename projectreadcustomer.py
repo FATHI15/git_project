06d55b85e919cb0054ba8073c7549413f249e6e8
fathima <fathimajamal10@gmail.com>
2018-07-09 14:34:47 +0530

    changes made to printing statemnt changes made to printing statemnt

--- projectreadcustomer.py
+++ projectreadcustomer.py
@@ -1,7 +1,7 @@
-name=input("enter the name")
-name1=input("enter the name")
-name2=input("enter the name")
-name3=input("enter the name")
+name=input("enter the name of the first customer")
+name1=input("enter the name of the second customer")
+name2=input("enter the name of the third customer")
+name3=input("enter the name of the fourth customer")
          
 print ('name', 'name1', 'name2', 'name3')
 
